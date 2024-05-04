72958fdbc19914e86b2a98e00de5545a82d1871a
dudematthew <mmoczydlowski4@gmail.com>
2024-05-04 14:31:17 +0200

    Fix nextAuth not registering; Update deps

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+const prisma = new PrismaClient();
+
+if (!process.env.secret) {
+    throw new Error("SECRET environment variable must be defined");
+}
+
+console.log('Auth has been found!');
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export {
+    handler as GET,
+    handler as POST,
+    handler as PATCH,
+    handler as PUT,
+    handler as DELETE,
+};
+  
